aee01baa7207f26d2658abba9b11902f792ca03a
Marwen-PC <marwen_abbes@hotmail.fr>
2025-11-20 22:25:56 +0100

    Add report import functionality to FHome form
    Introduced the ability to import and manage client and EMP reports:
    - Removed `Class1` from `Class1.cs`.
    - Added `Paths` class for directory management and `Enums` for report types.
    - Updated `FHome.Designer.cs` to include new controls (`compareButton`, `comparisonResultsGroupBox`, `comparisonDataGridView`) and reorganized layout.
    - Added event handlers for importing reports and interacting with status labels.
    - Implemented `CheckDirectories` to ensure directory structure and `ImportFile` for file import logic.
    - Updated `FHome.resx` and added a project reference to `Mira.Core` in `Mira.UI.csproj`.
    
    These changes improve the application's usability and maintainability.

---
+++ Mira.Core/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mira.Core
+{
+    public class Enums
+    {
+        public enum ReportType
+        {
+            Client,
+            EMP
+        };
+    }
+}
